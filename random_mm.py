8ac5733433efbdd3f43035fab8b99584f9388d32
Sloan Ahrens <sloan.ahrens@gmail.com>
2014-12-02 21:11:25 -0600

    testing

--- random_mm.py
+++ random_mm.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
         print("--- %s seconds ---" % elapsed)
 
         # system("~/spark/bin/spark-submit --master local[4] --jars ~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar ~/local_code/es-spark-matmult/es_spark_mm.py")
-        system("~/spark/bin/spark-submit --master spark://54.69.207.224:7077 --jars ~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar ~/es-spark-matmult/es_spark_mm.py")
+        system("~/spark/bin/spark-submit --master spark://ec2-54-69-207-224.us-west-2.compute.amazonaws.com:7077 --jars ~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar ~/es-spark-matmult/es_spark_mm.py")
 
 
 
