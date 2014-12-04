c3eb3f6d894d415d7b26d279dbcb3ea9ef234811
Sloan Ahrens <sloan.ahrens@gmail.com>
2014-12-03 18:10:49 -0600

    testing again

--- random_mm.py
+++ random_mm.py
@@ -117,11 +117,11 @@ if __name__ == '__main__':
         elapsed = round(time() - start_time, 2)
         print("--- %s seconds ---" % elapsed)
 
-        system("~/spark/bin/spark-submit --master local[4] --jars ~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar ~/local_code/es-spark-matmult/es_spark_mm.py")
-        # master_path = 'spark://ec2-54-69-207-224.us-west-2.compute.amazonaws.com:7077'
-        # jar_path = '~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar'
-        # code_path = '~/es-spark-matmult/es_spark_mm.py'
-        # system("~/spark/bin/spark-submit --master %s --jars %s %s" % (master_path, jar_path, code_path))
+        # system("~/spark/bin/spark-submit --master local[4] --jars ~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar ~/local_code/es-spark-matmult/es_spark_mm.py")
+        master_path = 'spark://ec2-54-69-207-224.us-west-2.compute.amazonaws.com:7077'
+        jar_path = '~/spark/jars/elasticsearch-hadoop-2.1.0.Beta2.jar'
+        code_path = '~/es-spark-matmult/es_spark_mm.py'
+        system("~/spark/bin/spark-submit --master %s --jars %s %s" % (master_path, jar_path, code_path))
 
 
 
